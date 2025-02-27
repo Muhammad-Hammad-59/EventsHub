c09ab0aa156c7021c0287e9c9bc729a0235ccc25
Muhammad Hammad <muhammadhammad010100@gmail.com>
2025-02-27 08:37:59 +0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+     <div className="text-center font-bold flex justify-center items-center bg-orange-900">
+      <h2>Hello Hammad </h2>
+     </div>
+  );
+}
